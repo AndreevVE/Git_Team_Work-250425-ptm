d7555fe74d7c90a94efed2266be781f7709b0194
SvetiKPa <t7svpa@gmail.com>
2025-07-30 10:39:18 +0200

    GH6_Svetlana

--- filemover.sh
+++ filemover.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#Описание: Скопировать все файлы с указанным расширением из исходной директории в целевую. 
+#file_extension
+#Действие: Копирование каждого файла с указанным расширением из исходной директории в целевую.
+# Вывести сообщение о копировании каждого файла
+
+#Исходная директория - source_directory Расширение - file_extension
+#Целевая директория - target_directory Новое расширение - new_file_extension
 
 # Запрос исходной и целевой директории у пользователя
 
@@ -12,6 +19,27 @@
 # Проверка, есть ли файлы с указанным расширением в исходной директории
 
 # Копирование файлов с указанным расширением в целевую директорию
+source_directory="$/Users/1/300725_old"
+target_directory="$/Users/1/300725_new"
+file_extension="sql"
+new_file_extension="txt"
+
+counter=0
+
+for file in "$source_directory"/*."$file_extension"
+do
+  if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        new_filename="${filename%$file_extension}$new_file_extension"
+        
+        cp "$file" "$target_directory/$new_filename"
+        echo "Скопирован: $filename -> $new_filename"
+        ((counter++))
+    fi
+
+done
+
+echo "Готово! Скопировано файлов: $counter"
 
 # Архивация исходных файлов.
 
